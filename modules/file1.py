e00402dcbbf2d82976ac7d275d41b1bc336940f4
Mayur Khemnar <120369158+mayur7922@users.noreply.github.com>
2025-01-09 13:38:46 +0530

    Add files via upload

---
+++ modules/file1.py
@@ -0,0 +1,5 @@
+def fun() :
+    print("Function is called")
+
+if __name__ == "__main__":
+    fun()
---
+++ modules/file2.py
@@ -0,0 +1,6 @@
+import file1
+
+print("Hello")
+
+# calling the function of other module i.e. file1
+file1.fun()
